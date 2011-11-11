445f71c8fd4137d2b1225d458b9c9e2a76ef243e
res09ggm <res09ggm@5d7aae61-8bab-4fb7-0488-007251116f01>
2011-11-11 05:34:02 +0000

    class hierarchy

---
+++ trunk/ ucimobile/Gravity Ball/Ability.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GravityBall
+{
+    interface Ability
+    {
+        int energyRequired
+        {
+            get;
+            set;
+        }
+
+        void upgrade();
+        void shoot();
+    }
+
+    class GravityBallProjectile : Ability 
+    {
+        double damagePerformed = 5.0;
+
+        public int energyRequired
+        {
+            get
+            {
+                return energyRequired;
+            }
+            set
+            {
+                energyRequired = value;
+            }
+        }
+
+        public void upgrade()
+        {
+            damagePerformed = damagePerformed * 1.25;
+            energyRequired = (int)((double)energyRequired * 1.25);
+        }
+
+        public void shoot()
+        {
+
+        }
+    }
+
+    public class GravitySphere : Ability
+    {
+        public int energyRequired
+        {
+            get
+            {
+                return energyRequired;
+            }
+            set
+            {
+                energyRequired = value;
+            }
+        }
+
+        public void upgrade()
+        {
+            energyRequired = (int)((double)energyRequired * 1.25);
+        }
+
+        public void shoot()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/ ucimobile/Gravity Ball/Player/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GravityBall
+{
+    class Player
+    {
+        private String name;
+        private int level;
+        private int exp;
+        private Double health;
+        private Double energy;
+        private ArrayList skills;
+
+        private Texture2D myTexture;
+        
+        public Player()
+        {
+            name = "Player 1";
+            initializePlayer();
+        }
+
+        public Player(String aName)
+        {
+            name = aName;
+            initializePlayer();
+        }
+
+        private Boolean initializePlayer()
+        {
+            level = 0;
+            exp = 0;
+            health = 100;
+            energy = 100;
+            skills = new ArrayList();
+
+            return true;
+        }
+
+        public void addAbility(Ability aAbility)
+        {
+            skills.Add(aAbility);
+        }
+
+        public Texture2D getTexture()
+        {
+            return myTexture;
+        }
+
+        public void setTexture(Texture2D aTexture)
+        {
+            myTexture = aTexture;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String aName)
+        {
+            name = aName;
+        }
+
+    }
+}
